1a5947ec70ba3a10b87e5212201c79fcd2bee7a5
Anagha Rao <anagha.rao79@gmail.com>
2024-07-02 10:19:38 +0530

    message

---
+++ imp.s
@@ -0,0 +1,41 @@
+	.text
+	.file	"imp.c"
+	.globl	main                            # -- Begin function main
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	movl	$0, -4(%rbp)
+	movl	$0, -8(%rbp)
+	movl	$2, -12(%rbp)
+	leaq	.L.str(%rip), %rdi
+	movl	$3, %esi
+	movb	$0, %al
+	callq	printf@PLT
+	movl	-8(%rbp), %eax
+	addl	-12(%rbp), %eax
+	addq	$16, %rsp
+	popq	%rbp
+	.cfi_def_cfa %rsp, 8
+	retq
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        # -- End function
+	.type	.L.str,@object                  # @.str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.L.str:
+	.asciz	"%d\n"
+	.size	.L.str, 4
+
+	.ident	"Ubuntu clang version 14.0.0-1ubuntu1.1"
+	.section	".note.GNU-stack","",@progbits
+	.addrsig
+	.addrsig_sym printf
